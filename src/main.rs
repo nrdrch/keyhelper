9d5f358c33215cc8cdf72780570829bb369fc9fd
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2024-04-15 00:12:40 +0200

    Update main.rs
    added Linux support

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,130 @@ use std::path::Path;
 use std::process::{Command, Stdio};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
-fn generate_ssh_key(file_name: &str, passphrase: Option<&str>) {
-    let user_profile = match env::var("USERPROFILE") {
-        Ok(val) => val,
-        Err(_) => {
-            print_error("Failed to fetch USERPROFILE environment variable.");
-            return;
-        }
-    };
 
-    let private_key_path = format!("{}\\.ssh\\{}", user_profile, file_name);
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        print_usage();
+        return;
+    }
+
+    let flag = &args[1];
+
+    if cfg!(target_os = "windows") {
+        main_windows(flag);
+    } else if cfg!(target_os = "linux") {
+        main_linux(flag);
+    } else {
+        println!("Unsupported operating system.");
+    }
+}
+
+// Implement other functions here
+
+
+// Linux main function
+fn main_linux(_flag: &str) {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        print_usage();
+        return;
+    }
+
+    let flag = &args[1];
+
+    match flag.as_str() {
+        "-n" => {
+            let file_name = &args[2];
+            let passphrase_index = args.iter().position(|arg| arg == "-p");
+            let passphrase = passphrase_index.and_then(|index| args.get(index + 1).map(|s| s.as_str()));
+            generate_ssh_key_linux(file_name, passphrase);
+        }
+        "-rm" => {
+            let file_name = &args[2];
+            remove_ssh_key_linux(file_name);
+        }
+        "-l" => list_files_in_dir_linux(),
+        "-c" => {
+            if args.len() < 3 {
+                print_error("Usage: -c <SSHKeyFileName> <User@IP>");
+                return;
+            }
+            let remote_host = &args.last().unwrap();
+            let ssh_key_name = if args.len() == 4 { args[2].as_str() } else { "" };
+            if ssh_key_name.is_empty() {
+                list_files_in_dir_linux();
+                return;
+            }
+            let file_name = match remote_host.split('@').nth(0) {
+                Some(name) => name,
+                None => {
+                    print_error("Invalid remote host format.");
+                    return;
+                }
+            };
+            copy_ssh_key_linux(remote_host, file_name);
+        }
+        _ => {
+            print_error("Invalid command. See usage:");
+            print_usage();
+        }
+    }
+}
+
+// Windows main function
+fn main_windows(_flag: &str) {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        print_usage();
+        return;
+    }
+
+    let flag = &args[1];
+
+    match flag.as_str() {
+        "-n" => {
+            let file_name = &args[2];
+            let passphrase_index = args.iter().position(|arg| arg == "-p");
+            let passphrase = passphrase_index.and_then(|index| args.get(index + 1).map(|s| s.as_str()));
+            generate_ssh_key_windows(file_name, passphrase);
+        }
+        "-rm" => {
+            let file_name = &args[2];
+            remove_ssh_key_windows(file_name);
+        }
+        "-l" => list_files_in_dir_windows(),
+        "-c" => {
+            if args.len() < 3 {
+                print_error("Usage: -c <SSHKeyFileName> <User@IP>");
+                return;
+            }
+            let remote_host = &args.last().unwrap();
+            let ssh_key_name = if args.len() == 4 { args[2].as_str() } else { "" };
+            if ssh_key_name.is_empty() {
+                list_files_in_dir_windows();
+                return;
+            }
+            let file_name = match remote_host.split('@').nth(0) {
+                Some(name) => name,
+                None => {
+                    print_error("Invalid remote host format.");
+                    return;
+                }
+            };
+            copy_ssh_key_windows(remote_host, file_name);
+        }
+        _ => {
+            print_error("Invalid command. See usage:");
+            print_usage();
+        }
+    }
+}
+
+
+fn generate_ssh_key_linux(file_name: &str, passphrase: Option<&str>) {
+    let user_profile = get_user_profile();
+    let private_key_path = format_ssh_path(&user_profile, file_name);
     let public_key_path = format!("{}.pub", private_key_path);
 
     if Path::new(&private_key_path).exists() || Path::new(&public_key_path).exists() {
@@ -25,21 +139,17 @@ fn generate_ssh_key(file_name: &str, passphrase: Option<&str>) {
         return;
     }
 
-    let mut powershell_command = format!("ssh-keygen -t rsa -f \"{}\"", private_key_path);
-
+    let mut ssh_keygen_command = Command::new("ssh-keygen");
+    ssh_keygen_command.arg("-t").arg("rsa").arg("-f").arg(&private_key_path);
     if let Some(pass) = passphrase {
         if !pass.is_empty() {
-            powershell_command.push_str(&format!(" -N \"{}\"", pass));
+            ssh_keygen_command.arg("-N").arg(pass);
         }
     } else {
-        powershell_command.push_str(" -N -");
+        ssh_keygen_command.arg("-N").arg("");
     }
 
-    let output = Command::new("powershell")
-        .arg("-Command")
-        .arg(&powershell_command)
-        .output()
-        .expect("Failed to execute PowerShell command");
+    let output = ssh_keygen_command.output().expect("Failed to execute ssh-keygen command");
 
     if output.status.success() {
         print_success(&format!("SSH key '{}' generated successfully.", file_name));
@@ -49,16 +159,9 @@ fn generate_ssh_key(file_name: &str, passphrase: Option<&str>) {
     }
 }
 
-fn remove_ssh_key(file_name: &str) {
-    let user_profile = match env::var("USERPROFILE") {
-        Ok(val) => val,
-        Err(_) => {
-            print_error("Failed to fetch USERPROFILE environment variable.");
-            return;
-        }
-    };
-
-    let private_key_path = format!("{}\\.ssh\\{}", user_profile, file_name);
+fn remove_ssh_key_linux(file_name: &str) {
+    let user_profile = get_user_profile();
+    let private_key_path = format_ssh_path(&user_profile, file_name);
     let public_key_path = format!("{}.pub", private_key_path);
 
     if !Path::new(&private_key_path).exists() && !Path::new(&public_key_path).exists() {
@@ -69,16 +172,11 @@ fn remove_ssh_key(file_name: &str) {
         return;
     }
 
-    let powershell_command = format!(
-        r#"Remove-Item "{}"; Remove-Item "{}""#,
-        private_key_path, public_key_path
-    );
-
-    let output = Command::new("powershell")
-        .arg("-Command")
-        .arg(&powershell_command)
+    let output = Command::new("rm")
+        .arg(&private_key_path)
+        .arg(&public_key_path)
         .output()
-        .expect("Failed to execute PowerShell command");
+        .expect("Failed to execute rm command");
 
     if output.status.success() {
         print_success(&format!(
@@ -91,16 +189,134 @@ fn remove_ssh_key(file_name: &str) {
     }
 }
 
-fn list_files_in_dir() {
-    let user_profile = match env::var("USERPROFILE") {
-        Ok(val) => val,
-        Err(_) => {
-            print_error("Failed to fetch USERPROFILE environment variable.");
+fn list_files_in_dir_linux() {
+    let user_profile = get_user_profile();
+    let dir_path = format!("{}/.ssh", user_profile);
+    let files = match fs::read_dir(&dir_path) {
+        Ok(files) => files,
+        Err(err) => {
+            print_error(&format!("Error listing files: {}", err));
             return;
         }
     };
 
-    let dir_path = format!("{}\\.ssh", user_profile);
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let mut cs = ColorSpec::new();
+    cs.set_fg(Some(Color::Ansi256(42))).set_bold(true);
+    stdout.set_color(&cs).unwrap();
+   
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let mut cs = ColorSpec::new();
+    cs.set_fg(Some(Color::Ansi256(42))).set_bold(true);
+    stdout.set_color(&cs).unwrap();
+    writeln!(&mut stdout, "\n>_SSH Key Helper ").unwrap();
+    stdout.reset().unwrap();
+    println!("   ______________________________________________________________");
+    println!("   Files in directory '{}':", dir_path);
+
+    for file in files {
+        if let Ok(file) = file {
+            let file_name = file.file_name().into_string().unwrap_or_else(|_| String::from("Invalid filename"));
+            let mut cs = ColorSpec::new();
+            cs.set_fg(Some(Color::Ansi256(42))).set_bold(true);
+            stdout.set_color(&cs).unwrap();
+            writeln!(&mut stdout, "   {}", file_name).unwrap();
+            stdout.reset().unwrap();
+        }
+    }
+}
+
+fn copy_ssh_key_linux(remote_host: &str, file_name: &str) {
+    let user_profile = get_user_profile();
+    let public_key_path = format!("{}/.ssh/{}.pub", user_profile, file_name);
+
+    if !Path::new(&public_key_path).exists() {
+        print_error(&format!("Public key for '{}' does not exist.", file_name));
+        return;
+    }
+
+    let output = Command::new("ssh-copy-id")
+        .arg(&format!("{}@{}", file_name, remote_host))
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .expect("Failed to execute ssh-copy-id command");
+
+    if output.status.success() {
+        print_success(&format!("SSH key copied successfully to {}.", remote_host));
+    } else {
+        let error_message = String::from_utf8_lossy(&output.stderr);
+        print_error(&format!("Error copying SSH key: {}", error_message));
+    }
+}
+
+fn generate_ssh_key_windows(file_name: &str, passphrase: Option<&str>) {
+    let user_profile = get_user_profile();
+    let private_key_path = format_ssh_path(&user_profile, file_name);
+    let public_key_path = format!("{}.pub", private_key_path);
+
+    if Path::new(&private_key_path).exists() || Path::new(&public_key_path).exists() {
+        print_error(&format!(
+            "SSH key with the name '{}' already exists. Please choose a different name.",
+            file_name
+        ));
+        return;
+    }
+
+    let mut ssh_keygen_command = Command::new("ssh-keygen");
+    ssh_keygen_command.arg("-t").arg("rsa").arg("-f").arg(&private_key_path);
+    if let Some(pass) = passphrase {
+        if !pass.is_empty() {
+            ssh_keygen_command.arg("-N").arg(pass);
+        }
+    } else {
+        ssh_keygen_command.arg("-N").arg("");
+    }
+
+    let output = ssh_keygen_command.output().expect("Failed to execute ssh-keygen command");
+
+    if output.status.success() {
+        print_success(&format!("SSH key '{}' generated successfully.", file_name));
+    } else {
+        let error_message = String::from_utf8_lossy(&output.stderr);
+        print_error(&format!("Error generating SSH key: {}", error_message));
+    }
+}
+
+fn remove_ssh_key_windows(file_name: &str) {
+    let user_profile = get_user_profile();
+    let private_key_path = format_ssh_path(&user_profile, file_name);
+    let public_key_path = format!("{}.pub", private_key_path);
+
+    if !Path::new(&private_key_path).exists() && !Path::new(&public_key_path).exists() {
+        print_error(&format!(
+            "SSH key with the name '{}' does not exist.",
+            file_name
+        ));
+        return;
+    }
+
+    let output = Command::new("del")
+        .arg(&private_key_path)
+        .arg(&public_key_path)
+        .output()
+        .expect("Failed to execute del command");
+
+    if output.status.success() {
+        print_success(&format!(
+            "SSH key '{}' and its public key removed successfully.",
+            file_name
+        ));
+    } else {
+        let error_message = String::from_utf8_lossy(&output.stderr);
+        print_error(&format!("Error removing SSH keys: {}", error_message));
+    }
+}
+
+fn list_files_in_dir_windows() {
+    let user_profile = get_user_profile();
+    let dir_path = format!("{}/.ssh", user_profile);
     let files = match fs::read_dir(&dir_path) {
         Ok(files) => files,
         Err(err) => {
@@ -130,16 +346,9 @@ fn list_files_in_dir() {
     }
 }
 
-fn copy_ssh_key(remote_host: &str, file_name: &str) {
-    let user_profile = match env::var("USERPROFILE") {
-        Ok(val) => val,
-        Err(_) => {
-            print_error("Failed to fetch USERPROFILE environment variable.");
-            return;
-        }
-    };
-
-    let public_key_path = format!("{}\\.ssh\\{}.pub", user_profile, file_name);
+fn copy_ssh_key_windows(remote_host: &str, file_name: &str) {
+    let user_profile = get_user_profile();
+    let public_key_path = format!("{}/.ssh/{}.pub", user_profile, file_name);
 
     if !Path::new(&public_key_path).exists() {
         print_error(&format!("Public key for '{}' does not exist.", file_name));
@@ -162,21 +371,16 @@ fn copy_ssh_key(remote_host: &str, file_name: &str) {
     }
 }
 
-fn print_error(message: &str) {
-    let mut stderr = StandardStream::stderr(ColorChoice::Always);
-    stderr.set_color(ColorSpec::new().set_fg(Some(Color::Red))).unwrap();
-    writeln!(&mut stderr, "{}", message).unwrap();
-    stderr.reset().unwrap();
-}
 
-fn print_success(message: &str) {
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green))).unwrap();
-    writeln!(&mut stdout, "{}", message).unwrap();
-    stdout.reset().unwrap();
-}
 
+
+fn format_ssh_path(user_profile: &str, file_name: &str) -> String {
+    format!("{}/.ssh/{}", user_profile, file_name)
+}
 fn print_usage() {
+    let _os_name = if cfg!(target_os = "windows") { "Windows" } else { "Linux" };
+    let _ssh_dir = if cfg!(target_os = "windows") { "C:\\Users\\<user>\\.ssh" } else { "/home/<user>/.ssh" };
+
     let header = ">_SSH Key Helper ";
     
     let cmd_descs = [
@@ -218,46 +422,34 @@ fn print_usage() {
     writeln!(&mut stdout, "{}", footer).unwrap();
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        print_usage();
-        return;
-    }
 
-    let flag = &args[1];
-
-    if flag == "-n" {
-        let file_name = &args[2];
-        let passphrase_index = args.iter().position(|arg| arg == "-p");
-        let passphrase = passphrase_index.and_then(|index| args.get(index + 1).map(|s| s.as_str()));
-        generate_ssh_key(file_name, passphrase);
-    } else if flag == "-rm" {
-        let file_name = &args[2];
-        remove_ssh_key(file_name);
-    } else if flag == "-l" {
-        list_files_in_dir();
-    } else if flag == "-c" {
-        if args.len() < 3 {
-            print_error("Usage: -c <SSHKeyFileName> <User@IP>");
-            return;
-        }
-        let remote_host = &args.last().unwrap();
-        let ssh_key_name = if args.len() == 4 { args[2].as_str() } else { "" };
-        if ssh_key_name.is_empty() {
-            list_files_in_dir();
-            return;
-        }
-        let file_name = match remote_host.split('@').nth(0) {
-            Some(name) => name,
-            None => {
-                print_error("Invalid remote host format.");
-                return;
+fn get_user_profile() -> String {
+    if cfg!(target_os = "windows") {
+        match env::var("USERPROFILE") {
+            Ok(val) => val,
+            Err(_) => {
+                print_error("Failed to fetch USERPROFILE environment variable.");
+                std::process::exit(1);
             }
-        };
-        copy_ssh_key(remote_host, file_name);
+        }
+    } else if cfg!(target_os = "linux") {
+        match env::var("HOME") {
+            Ok(val) => val,
+            Err(_) => {
+                print_error("Failed to fetch HOME environment variable.");
+                std::process::exit(1);
+            }
+        }
     } else {
-        print_error("Invalid command. See usage:");
-        print_usage();
+        // Handle other OS cases
+        unimplemented!();
     }
+}
+
+fn print_error(message: &str) {
+    eprintln!("Error: {}", message);
+}
+
+fn print_success(message: &str) {
+    println!("Success: {}", message);
 }
